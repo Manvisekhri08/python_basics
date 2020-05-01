e39bc74727837f0c208681d1ac459497287f762b
Manvisekhri08 <manvisekhri08@gmail.com>
2020-05-01 11:24:10 +0530

    Update listtypo.py

--- listtypo.py
+++ listtypo.py
@@ -1,5 +1,6 @@
-student_grade=[20.1,30.3,31]
-mysum=sum(student_grade)
+temp_monday=[20.1,30.3,31]
+student_grade={"Marie": 20.1,"John": 30.3, "Sam": 31}
+mysum=sum(student_grade.values())
 length=len(student_grade)
 mean=mysum/length
 print(mean)
